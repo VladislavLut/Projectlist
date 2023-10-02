230b2a3846626a7d51a6431519d8f4d7c389ffd6
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-02 20:15:07 +0300

    ex-2 was done

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+text = input('Введіть текст: ')
+reserved_words = input('Введіть список зарезервованих слів: ').split()
 
+for word in text.split():
+    if word in reserved_words:
+        text = text.replace(word, word.upper())
+
+print(text)
